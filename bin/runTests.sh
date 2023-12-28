9244b50a99a2a7ff84c97865b55613d815b4e4e6
Jeff Wu <jeff@notional.finance>
2023-12-28 05:06:19 -0800

    fix: adding patch and test script

--- bin/runTests.sh
+++ bin/runTests.sh
@@ -13,4 +13,9 @@ brownie test tests/stateful/vaults --disable-warnings
 brownie test tests/stateful/test_!(settlement).py --disable-warnings
 brownie test tests/stateful/test_settlement.py --disable-warnings
 brownie test tests/mainnet-fork/test_treasury_action.py --network mainnet-fork
-# export CHAIN_ID=42161; brownie test tests/arbitrum-fork/test_pcash_rebalancing.py --network arbitrum-fork
+
+# Creates a patch file for a given network:
+# git diff HEAD^ contracts/global/Deployments.sol > bin/arb.patch
+
+git apply bin/arb.patch
+forge test
