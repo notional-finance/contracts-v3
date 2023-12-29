07da24f030f37391cd71b7e5c0a002eea37e77c1
Jeff Wu <jeff@notional.finance>
2023-12-29 06:30:53 -0800

    deploy: secondary rewarder and usdc migration

---
+++ scripts/deployers/rewarder_deployer.py
@@ -0,0 +1,27 @@
+from brownie import SecondaryRewarder, accounts
+from scripts.inspect import get_addresses
+
+rewarderConfig = {
+    'currencyId': 8,
+    'rewardToken': '0x912CE59144191C1204E64559FE8253a0e49E6548', # ARB
+    'initialEmissionRate': 1e8,
+    'endTime': 1704441600, # Jan 5 00:00:00
+}
+
+def main():
+    (_, notional, *_) =get_addresses()
+    deployer = accounts.load('DEPLOYER')
+
+    rewarder = SecondaryRewarder.deploy(
+        notional.address,
+        rewarderConfig['currencyId'],
+        rewarderConfig['rewardToken'],
+        rewarderConfig['initialEmissionRate'],
+        rewarderConfig['endTime'],
+        {"from": deployer}
+    )
+
+    notional.setSecondaryIncentiveRewarder(
+        rewarderConfig['currencyId'], rewarder,
+        {"from": notional.owner()}
+    )
