5b9147338c9ea133a89e71e86fa16520e8535365
Jeff Wu <jeff@notional.finance>
2023-12-29 09:48:27 -0800

    fix: adding rewarder test

--- scripts/deployers/rewarder_deployer.py
+++ scripts/deployers/rewarder_deployer.py
@@ -4,7 +4,7 @@ from scripts.inspect import get_addresses
 rewarderConfig = {
     'currencyId': 8,
     'rewardToken': '0x912CE59144191C1204E64559FE8253a0e49E6548', # ARB
-    'initialEmissionRate': 1e8,
+    'initialEmissionRate': 52e8, # Approx 1e8 ARB per week
     'endTime': 1704441600, # Jan 5 00:00:00
 }
 
