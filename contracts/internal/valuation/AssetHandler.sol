a0d4f3d373070849cc2e4f1f4109e245636ac4bf
Jeff Wu <jeff@notional.finance>
2024-01-02 10:34:42 -0800

    Bugfix: debt rounding errors (#9)

--- contracts/internal/valuation/AssetHandler.sol
+++ contracts/internal/valuation/AssetHandler.sol
@@ -77,7 +77,10 @@ library AssetHandler {
         int256 discountFactor = getDiscountFactor(timeToMaturity, oracleRate);
 
         require(discountFactor <= Constants.RATE_PRECISION); // dev: get present value invalid discount factor
-        return notional.mulInRatePrecision(discountFactor);
+
+        int256 pv = notional.mulInRatePrecision(discountFactor);
+        // Ensure that any debt balance always returns a -1 value.
+        return notional < 0 ?  SafeInt256.min(pv, -1) : pv;
     }
 
     function getRiskAdjustedfCashDiscount(
@@ -118,7 +121,10 @@ library AssetHandler {
             getRiskAdjustedDebtDiscount(cashGroup, maturity, blockTime);
 
         require(discountFactor <= Constants.RATE_PRECISION); // dev: get risk adjusted pv, invalid discount factor
-        return notional.mulInRatePrecision(discountFactor);
+        int256 pv = notional.mulInRatePrecision(discountFactor);
+
+        // Ensure that any debt balance always returns a -1 value.
+        return notional < 0 ?  SafeInt256.min(pv, -1) : pv;
     }
 
     /// @notice Returns the non haircut claims on cash and fCash by the liquidity token.
