4c1cb48415f1b11b32755de2407b3195a09b0e61
sbuljac <sbuljac@gmail.com>
2024-02-01 00:39:54 +0100

    Audit: Fix issue #49
    /sherlock-audit/2023-12-notional-update-5-judging/issues/49

--- interfaces/aave/ILendingPool.sol
+++ interfaces/aave/ILendingPool.sol
@@ -57,29 +57,22 @@ interface ILendingPool {
    **/
   function getReserveData(address asset) external view returns (ReserveData memory);
 
-  // refer to the whitepaper, section 1.1 basic concepts for a formal description of these properties.
   struct ReserveData {
-    //stores the reserve configuration
     ReserveConfigurationMap configuration;
-    //the liquidity index. Expressed in ray
     uint128 liquidityIndex;
-    //variable borrow index. Expressed in ray
-    uint128 variableBorrowIndex;
-    //the current supply rate. Expressed in ray
     uint128 currentLiquidityRate;
-    //the current variable borrow rate. Expressed in ray
+    uint128 variableBorrowIndex;
     uint128 currentVariableBorrowRate;
-    //the current stable borrow rate. Expressed in ray
     uint128 currentStableBorrowRate;
     uint40 lastUpdateTimestamp;
-    //tokens addresses
+    uint16 id;
     address aTokenAddress;
     address stableDebtTokenAddress;
     address variableDebtTokenAddress;
-    //address of the interest rate strategy
     address interestRateStrategyAddress;
-    //the id of the reserve. Represents the position in the list of the active reserves
-    uint8 id;
+    uint128 accruedToTreasury;
+    uint128 unbacked;
+    uint128 isolationModeTotalDebt;
   }
 
   struct ReserveConfigurationMap {
