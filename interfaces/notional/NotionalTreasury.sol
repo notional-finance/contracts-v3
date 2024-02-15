c57cbf38d9420dd7907572042f38a79ed2d3b893
sbuljac <sbuljac@gmail.com>
2024-02-15 17:25:50 +0100

    fix: revert on residual in tests

--- interfaces/notional/NotionalTreasury.sol
+++ interfaces/notional/NotionalTreasury.sol
@@ -48,7 +48,7 @@ interface NotionalTreasury {
 
     function setRebalancingCooldown(uint16 currencyId, uint40 cooldownTimeInSeconds) external;
 
-    function checkRebalance() external returns (uint16[] memory currencyIds);
+    function checkRebalance() external view  returns (uint16[] memory currencyIds);
 
     function rebalance(uint16 currencyId) external;
 
