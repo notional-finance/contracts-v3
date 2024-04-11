799032cd3e727885cb7b3baf60096177519c15ac
Jeff Wu <jeff@notional.finance>
2024-04-11 08:44:15 -0700

    deploy: ezETH and weETH oracles on arbitrum

--- scripts/inspect.py
+++ scripts/inspect.py
@@ -1,8 +1,13 @@
 # flake8: noqa
 import json
+import math
 from brownie import NoteERC20, Router, network, interface
+from brownie.network import Chain
 from brownie.network.contract import Contract
+from tests.constants import SECONDS_IN_YEAR
 from tests.helpers import get_balance_action, get_balance_trade_action
+
+chain = Chain()
 
 def get_router_args(router):
     return [
@@ -28,6 +33,32 @@ def get_multicall():
     return Contract.from_abi(
         "Multicall", "0xcA11bde05977b3631167028862bE2a173976CA11", multicall_abi
     )
+
+def get_ntoken_spot_value(notional, currencyId):
+    address = notional.nTokenAddress(currencyId)
+    (_, fCashAssets) = notional.getNTokenPortfolio(address)
+    markets = notional.getActiveMarkets(currencyId)
+    ntoken = notional.getNTokenAccount(address)
+    totalPCash = ntoken['cashBalance']
+    totalUnderlying = 0
+    oracleValue = notional.nTokenPresentValueUnderlyingDenominated(currencyId)
+
+    for i in range(0, len(fCashAssets)):
+        netFCash = fCashAssets[i][3] + markets[i][2]
+        timeToMaturity = markets[i][1] - chain.time()
+        discountFactor = math.exp(-(markets[i][5] * timeToMaturity) / (1e9 * SECONDS_IN_YEAR))
+        totalUnderlying += netFCash * discountFactor
+        totalPCash += markets[i][3]
+
+    totalUnderlying += notional.convertCashBalanceToExternal(currencyId, totalPCash, True) * 1e8 / 1e6
+    return (totalUnderlying / ntoken['totalSupply'], oracleValue / ntoken['totalSupply'], totalUnderlying, oracleValue, ntoken['totalSupply'])
+
+def mint_ntokens(notional, currencyId, account, amount):
+    # >>> whale = '0xb38e8c17e38363af6ebdcb3dae12e0243582891d'
+    # >>> usdt = Contract.from_explorer('0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9')
+    notional.batchBalanceAction(account, [
+        get_balance_action(currencyId, "DepositUnderlyingAndMintNToken", depositActionAmount=amount)
+    ], {"from": account})
 
 def get_addresses():
     networkName = network.show_active()
--- scripts/mainnet/eth_oracles.py
+++ scripts/mainnet/eth_oracles.py
@@ -1,9 +1,12 @@
 from brownie import ZERO_ADDRESS, accounts
 from scripts.deployers.oracle_deployer import deploy_chainlink_usd_oracle
-from scripts.mainnet.eth_config import ChainlinkOracles, ListedTokens
+# from scripts.mainnet.eth_config import ChainlinkOracles
+from scripts.arbitrum.arb_config import ChainlinkOracles
 
 DEPLOYER = "0x8B64fA5Fd129df9c755eB82dB1e16D6D0Bdf5Bc3"
 
+# Mainnet: 0xCa140AE5a361b7434A729dCadA0ea60a50e249dd
+# Arbitrum: 0x58784379C844a00d4f572917D43f991c971F96ca
 ezETH_USD = {
     'oracleType': 'ChainlinkAdapter',
     'baseOracle': ChainlinkOracles['ezETH/ETH'],
@@ -13,6 +16,8 @@ ezETH_USD = {
     'sequencerUptimeOracle': ZERO_ADDRESS
 }
 
+# Mainnet: 0xdDb6F90fFb4d3257dd666b69178e5B3c5Bf41136
+# Arbitrum: 0x9414609789C179e1295E9a0559d629bF832b3c04
 weETH_USD = {
     'oracleType': 'ChainlinkAdapter',
     'baseOracle': ChainlinkOracles['weETH/ETH'],
@@ -24,19 +29,6 @@ weETH_USD = {
 
 def main():
     deployer = accounts.load("MAINNET_DEPLOYER")
-    # wbtc = deploy_chainlink_usd_oracle("WBTC", deployer, ListedTokens["WBTC"]["usdOracle"])
-    # rETH = deploy_chainlink_usd_oracle("rETH", deployer, ListedTokens["rETH"]["usdOracle"])
-    # osETH = deploy_chainlink_usd_oracle(
-    #     "osETH",
-    #     deployer,
-    #     {
-    #         "oracleType": "ChainlinkAdapter",
-    #         "baseOracle": ChainlinkOracles["osETH/ETH"],
-    #         "quoteOracle": ChainlinkOracles["ETH/USD"],
-    #         "invertBase": False,
-    #         "invertQuote": True,
-    #         "sequencerUptimeOracle": ZERO_ADDRESS,
-    #     },
-    # )
+    # deployer = DEPLOYER
     ezETH = deploy_chainlink_usd_oracle("ezETH", deployer, ezETH_USD)
     weETH = deploy_chainlink_usd_oracle("weETH", deployer, weETH_USD)
